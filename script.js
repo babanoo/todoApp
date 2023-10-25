d26dfb902cab29e5ca2fefb0fc32d98677a44068
babanoo <salwababano31@gmail.com>
2023-10-25 19:54:37 +0100

    summary

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addTaskElement = document.querySelector(".add-task");
 const tasksWrapper = document.querySelector(".tasks-wrapper");
-const tasks = [
+const countImportant = document.querySelector("#count-important");
+const countTasks = document.querySelector("#count-Tasks");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [
   {
     id: "t4",
     title: "task four",
@@ -27,31 +29,63 @@ const tasks = [
   },
 ];
 renderTasks(tasks);
-
+getDataFromLocalStorage();
+function saveCountTask() {
+  countTasks.textContent = tasks.filter((task) => task).length;
+  addDataToLocalStorage(tasks);
+}
+saveCountTask();
 function renderTasks(tasks) {
   tasksWrapper.innerHTML = "";
   tasks.forEach((task) => {
-    const div = document.createElement("div");
-    div.className =
+    const todoContainer = document.createElement("div");
+    todoContainer.className =
       "task py-3 d-flex align-items-start border-bottom cursor-pointer position-relative";
-    div.setAttribute("data-id", task.id);
+    todoContainer.setAttribute("data-id", task.id);
     const taskContent = document.createElement("p");
     taskContent.className = "flex-grow-1 ps-5 content";
-    div.appendChild(taskContent);
+    todoContainer.appendChild(taskContent);
     taskContent.appendChild(document.createTextNode(task.title));
     const trashButton = document.createElement("button");
     trashButton.className = "bi bi-trash border-0 bg-body";
-    div.appendChild(trashButton);
+    todoContainer.appendChild(trashButton);
     const startButton = document.createElement("button");
-    div.appendChild(startButton);
+    todoContainer.appendChild(startButton);
     startButton.className = "bi bi-star border-0 bg-body";
-    tasksWrapper.prepend(div);
+    if (task.important === true) {
+      startButton.className = "bi bi-star-fill";
+    }
+    startButton.addEventListener("click", (e) => {
+      if (e.target.classList.contains("bi-star")) {
+        e.target.classList.replace("bi-star", "bi-star-fill");
+        e.target.classList.add("text-primary");
+      } else {
+        e.target.classList.replace("bi-star-fill", "bi-star");
+        e.target.classList.remove("text-primary");
+      }
+    });
+    tasksWrapper.prepend(todoContainer);
   });
 }
+
+tasksWrapper.addEventListener("click", (e) => {
+  if (e.target.classList.contains("task")) {
+    e.target.classList.toggle("done");
+  }
+  if (e.target.classList.contains("bi-trash")) {
+    const confirmDelete = confirm("are you sure");
+    if (confirmDelete) {
+      e.target.parentElement.remove();
+      removeTask(e.target.parentElement.getAttribute("data-id"));
+    }
+  }
+});
+
 addTaskElement.addEventListener("click", (e) => {
   const promptMsgTask = prompt("Enter Task");
   if (promptMsgTask.trim() !== "") {
     createNewTask(promptMsgTask);
+    addDataToLocalStorage(tasks);
   } else {
     alert("Please add some task!");
     return false;
@@ -67,6 +101,23 @@ function createNewTask(taskText) {
   };
   tasks.push(newTask);
   renderTasks(tasks);
+  addDataToLocalStorage(tasks);
+  saveCountTask();
+}
+function addDataToLocalStorage(tasks) {
+  window.localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    renderTasks(tasks);
+  }
+}
+function removeTask(taskId) {
+  tasks = tasks.filter((task) => task.id != taskId);
+  addDataToLocalStorage(tasks);
+  saveCountTask();
 }
 /*=============== SEARCH BAR JS ===============*/
 
